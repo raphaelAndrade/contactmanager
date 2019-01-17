7436b6a5d8960023edb436e6f9746c9687dc06bf
raphaelAndrade <raphaelsantosdesigner@gmail.com>
2019-01-16 16:35:57 -0800

    Updates

---
+++ precache-manifest.59e50758bffc9d4f3fc5578f912d4854.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "de8e7900637940b38c7e",
+    "url": "/contactmanager/static/js/main.de8e7900.chunk.js"
+  },
+  {
+    "revision": "fb91d0c9c229a8fc7b9c",
+    "url": "/contactmanager/static/js/1.fb91d0c9.chunk.js"
+  },
+  {
+    "revision": "de8e7900637940b38c7e",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "fb91d0c9c229a8fc7b9c",
+    "url": "/contactmanager/static/css/1.64dd0039.chunk.css"
+  },
+  {
+    "revision": "d598bcf4b76549835f39d9a1cc35033d",
+    "url": "/contactmanager/index.html"
+  }
+];
